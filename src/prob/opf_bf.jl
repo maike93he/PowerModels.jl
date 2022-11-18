2ae19101af5a7aca5200bc634b459fb53125b7ed
Maike Held <Maike.Held@rl-institut.de>
2022-11-18 06:55:30 +0100

    Minor changes

--- src/prob/opf_bf.jl
+++ src/prob/opf_bf.jl
@@ -111,10 +111,10 @@ end
 function build_mn_opf_bf_flex(pm::AbstractPowerModel)
     for (n, network) in nws(pm)
         # VARIABLES
-        if (ref(pm, 1, :opt_version) == 1)
+        if (ref(pm, 1, :opt_version) == 1)|(ref(pm, 1, :opt_version) == 3)
             variable_branch_power_radial(pm, nw=n, bounded=false)
             variable_branch_current(pm, nw=n, bounded=false)
-        elseif (ref(pm, 1, :opt_version) == 2)|(ref(pm, 1, :opt_version) == 3)
+        elseif (ref(pm, 1, :opt_version) == 2)
             variable_branch_power_radial(pm, nw=n)  # Eq. ():  branch power <= rate_a (s_nom)
             variable_branch_current(pm, nw=n)  # Eq. ()
             # variable_slack_grid_restrictions(pm, nw=n)  # TODO
