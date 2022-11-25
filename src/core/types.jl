d199c88fc0ae42abff962476dd37040ecfeb388e
Maike Held <Maike.Held@rl-institut.de>
2022-11-25 17:29:49 +0100

    Added non convex implementation of radial branch flow model

--- src/core/types.jl
+++ src/core/types.jl
@@ -428,10 +428,20 @@ mutable struct SOCBFConicPowerModel <: AbstractSOCBFConicModel @pm_fields end
 
 ""
 
-abstract type AbstractSOCBFModelEdisgo <: AbstractBFQPModel end
+abstract type AbstractBFModelEdisgo <: AbstractBFQPModel end
 
 """
-Second-order cone relaxation of branch flow model (eDisGo implementation)
+Radial branch flow model (eDisGo implementation)
+
+
+Applicable to problem formulations with `_bf` in the name.
+"""
+mutable struct BFPowerModelEdisgo <: AbstractBFModelEdisgo @pm_fields end
+
+abstract type AbstractSOCBFModelEdisgo <: AbstractBFModelEdisgo end
+
+"""
+Second-order cone relaxation of radial branch flow model (eDisGo implementation).
 
 
 Applicable to problem formulations with `_bf` in the name.
@@ -439,6 +449,15 @@ Applicable to problem formulations with `_bf` in the name.
 mutable struct SOCBFPowerModelEdisgo <: AbstractSOCBFModelEdisgo @pm_fields end
 
 
+abstract type AbstractNCBFModelEdisgo <: AbstractBFModelEdisgo end
+
+"""
+Non convex radial branch flow model (eDisGo implementation).
+
+
+Applicable to problem formulations with `_bf` in the name.
+"""
+mutable struct NCBFPowerModelEdisgo <: AbstractNCBFModelEdisgo @pm_fields end
 
 
 ###### SDP Relaxations ######
