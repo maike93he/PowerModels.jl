c0f25fc5b1783d9fbcc508e2d91059ec2a9fa95e
Maike Held <Maike.Held@rl-institut.de>
2023-01-15 13:45:31 +0100

    changed OF parameters

--- src/core/objective.jl
+++ src/core/objective.jl
@@ -657,10 +657,11 @@ function objective_min_losses_slacks(pm::AbstractBFModelEdisgo)
     pcps = Dict(n => var(pm, n, :pcps) for n in nws)
     s_base = ref(pm, 1, :baseMVA)
     l = Dict(n => Dict(i => get(branch, "length", 1.0) for (i,branch) in ref(pm, n, :branch)) for n in nws)
-    c = Dict(n => Dict(i => get(branch, "cost", 1.0) for (i,branch) in ref(pm, n, :branch)) for n in nws)
+    c = Dict(n => Dict(i => get(branch, "cost_factor", 1.0) for (i,branch) in ref(pm, n, :branch)) for n in nws)
+
 
     return JuMP.@objective(pm.model, Min,
-        1e3 * s_base * sum(sum(ccm[n][b]*r[n][b]*l[n][b]*c[n][b] for (b,i,j) in ref(pm, n, :arcs_from)) for n in nws) # minimize line losses
+        1e6 * s_base * sum(sum(ccm[n][b]*r[n][b]*c[n][b] for (b,i,j) in ref(pm, n, :arcs_from)) for n in nws) # minimize line losses
         + 1e6 * s_base * sum(sum(pgc[n]) for n in nws) # minimize non-dispatchable curtailment
         + 1e6 * s_base * sum(sum(pgens[n]) for n in nws) # minimize dispatchable curtailment
         #+ s_base * sum(sum(phps[n]) for n in nws) # minimize heatpump slack variables
